808489f7a855690c5927919910b19e3aef183e9c
caroMaio <carolinabelenmaio@gmail.com>
2020-09-22 17:16:57 -0300

    Navbar and principal title  with styles and props on buttons

---
+++ src/Components/NavbarNavegation.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import styled from 'styled-components'
+
+
+const NavbarNavegation = () => {
+    return (
+        <StyledNavbar sticky="top" collapseOnSelect expand="md" variant="light">
+        <StyleHomeButton href="#home">Kicz Oeste</StyleHomeButton>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+                <Nav.Link href="#features">Inicio</Nav.Link>
+                <Nav.Link href="#">Productos</Nav.Link>
+                <Nav.Link href="#pricing">Contáctanos ahora</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+        </StyledNavbar>
+    );
+};
+
+export default NavbarNavegation;
+
+const StyledNavbar = styled(Navbar)`
+    background: #f6f6f6;
+    font-family: 'Spartan', sans-serif;
+`;
+
+const StyleHomeButton = styled(Navbar.Brand) `{
+    font-weight: 600;
+}`
