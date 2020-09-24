0948d103e30b286ab7683d5b0e7196cbb3543440
caroMaio <carolinabelenmaio@gmail.com>
2020-09-24 12:45:32 -0300

    List in boxList file modified. Add array and map for each ListText

--- src/Components/PrincipalPage/BoxList/BoxList.js
+++ src/Components/PrincipalPage/BoxList/BoxList.js
@@ -1,25 +1,63 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ListText from './ListText';
-import ImageNike from '../../../Image/nike.jpg'
+import ImageNike from '../../../Image/nike.jpg';
+import styled, { keyframes } from 'styled-components';
 
+    const BoxList = () => {
 
-class BoxList extends Component {
-    render() {
+        const phrases = ['Envíos a todo el país', 'Indumentaria y calzado', 'Accesorios'];
+
         return (
             <div>
                 <ul className="text-center mx-auto my-3 p-0">
-                    <ListText text = 'Envíos a todo el país' />
-                    <ListText text = 'Indumentaria y calzado' />
-                    <ListText text = 'Accesorios' />
+   
+                   {
+                    phrases.map((prhase, index) => (
+                        <ListText key = {index}
+                                  text = {prhase}
+                        />  
+                    ))
+                    }
                 </ul>
-                <div class="container">
-                    <img className="w-100 my-4" src= {ImageNike } alt="Img zapatillas nike" />
+                <div className="container col-12 col-md-4">
+                    <StyleButton className="position-relative d-flex align-items-center justify-content-center border-0 p-0">
+                        <PlayIcon className="position-absolute bg-white play-icon rounded-circle d-flex justify-content-center">
+                            <i className="icon ion-md-play align-self-center"></i>
+                        </PlayIcon>
+                        <img className="w-100 my-4" src= {ImageNike } alt="Img zapatillas nike" />
+                    </StyleButton>
                 </div>
             </div>
 
         );
+    
     };
-};
 
 export default BoxList;
 
+const StyleButton = styled.div`{
+    background: transparent;
+}`;
+
+
+const shadowButton = keyframes`
+    0% {
+        box-shadow: 0 0 0 0 rgba(255, 255, 255, .8);  
+    }
+
+    70%{
+        box-shadow: 0 0 0 25px rgba(255, 255, 255, 0);      
+    }
+
+    100% {
+        box-shadow: 0 0 0 25px rgba(255, 255, 255, 0);        
+    }
+`;
+
+const PlayIcon = styled.div`{
+    width: 5rem;
+    height: 5rem;
+    box-shadow: 0 0 0 0 rgba(255, 255, 255, .5);
+    color: black;
+    animation: ${shadowButton} 1.5s infinite;
+}`;
--- src/Components/PrincipalPage/BoxList/ListText.js
+++ src/Components/PrincipalPage/BoxList/ListText.js
@@ -4,7 +4,7 @@ class ListText extends Component {
     render() {
         return (
             <p className="font-weight-bold p-0 m-0 pb-3">
-                <i class="icon ion-md-checkmark mr-2"></i>
+                <i className="icon ion-md-checkmark mr-2"></i>
                 {this.props.text}
             </p>
         )
